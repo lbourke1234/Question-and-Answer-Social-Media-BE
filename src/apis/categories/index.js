c0d9ec2e2ea48b41952b76681092c5c17283bad7
lbourke1234 <lbourke1234@gmail.com>
2022-07-22 14:56:18 +0100

    push after meeting

--- src/apis/categories/index.js
+++ src/apis/categories/index.js
@@ -3,4 +3,25 @@ import CategoriesModel from './model.js'
 
 const router = express.Router()
 
+router.get('/', async (req, res, next) => {
+  try {
+    const categories = await CategoriesModel.find()
+    res.send(categories)
+  } catch (error) {
+    console.log(error)
+    next(error)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const newCategory = new CategoriesModel(req.body)
+    const savedCategory = await newCategory.save()
+    res.send(savedCategory)
+  } catch (error) {
+    console.log(error)
+    next(error)
+  }
+})
+
 export default router
--- src/apis/categories/model.js
+++ src/apis/categories/model.js
@@ -6,8 +6,8 @@ const categoriesSchema = new Schema({
   name: { type: String, required: true },
   image: { type: String, default: 'https://cdn.onlinewebfonts.com/svg/img_98811.png' },
   description: { type: String, required: true },
-  followers: [{ type: mongoose.Types.ObjectId, ref: 'User' }],
-  questions: [{ type: mongoose.Types.ObjectId, ref: 'Question' }]
+  followers: [{ type: mongoose.Types.ObjectId, default: [], ref: 'User' }],
+  questions: [{ type: mongoose.Types.ObjectId, default: [], ref: 'Question' }]
 })
 
 export default model('Category', categoriesSchema)
